4e5f127138c3c8333967f57a9f7ab3b6d644969b
Victor Reisenauer <victorreisenauer99@gmail.com>
2020-04-14 17:29:21 +0200

    feat: Adds blocDelegate to monitor state changes for debugging

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:lrs_app_v3/presenation/lrs_app.dart';
 import 'package:lrs_app_v3/injection.dart';
 import 'package:injectable/injectable.dart';
+import 'package:lrs_app_v3/simple_bloc_delegate.dart';
 
 
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  BlocSupervisor.delegate = SimpleBlocDelegate();
   configureInjection(Environment.test);
   runApp(LrsApp());
 }
---
+++ lib/simple_bloc_delegate.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+export 'package:bloc/bloc.dart';
+
+/// Prints out all state changes to terminal for debugging. 
+class SimpleBlocDelegate extends BlocDelegate {
+  @override
+  void onEvent(Bloc bloc, Object event) {
+    super.onEvent(bloc, event);
+    print('onEvent $event');
+  }
+
+  @override
+  onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print('onTransition $transition');
+  }
+
+  @override
+  void onError(Bloc bloc, Object error, StackTrace stacktrace) {
+    super.onError(bloc, error, stacktrace);
+    print('onError $error');
+  }
+}
