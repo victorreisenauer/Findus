4b828f472466746235a1f4c0db5240e6fef6379b
Victor Reisenauer <victorreisenauer99@gmail.com>
2020-04-15 21:25:14 +0200

    fix: Makes profile picture only appear when user is logged in

--- lib/presenation/pages/home/home_page.dart
+++ lib/presenation/pages/home/home_page.dart
@@ -2,16 +2,13 @@ import 'package:flutter/material.dart';
 import 'widgets/logout_button.dart';
 
 class HomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('HomePage'),
       ),
-      body: Center(
-        child: LogoutButton()
-      ),
+      body: Center(child: LogoutButton()),
     );
   }
 }
--- lib/presenation/pages/welcome/welcome_page.dart
+++ lib/presenation/pages/welcome/welcome_page.dart
@@ -4,102 +4,151 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lrs_app_v3/application/auth/auth_bloc.dart';
 import 'package:lrs_app_v3/presenation/routes/router.gr.dart';
 import 'package:lrs_app_v3/presenation/pages/core/background_image.dart';
-import 'package:lrs_app_v3/domain/core/value_objects.dart';
-
 
 class WelcomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    String userGreeting;
-    String signInOrChangeUser;
-    var profilePicture;
     return Scaffold(
-      body: BlocConsumer<AuthBloc, AuthState> (
-        listener: (context, state) {
-        },
-        builder: (context, state)  {
-          state.map(
-            initial: (_) {}, 
-            authenticated: (obj) {
-              var valueOrFailure = obj.user.name.value.fold(
-                (f) => f,
-                (v) => v,
-                );
-              userGreeting = "Hey, $valueOrFailure!";     // TODO: fix string representation of names etc
-              signInOrChangeUser = "Nutzer wechseln";
-            },
-            unauthenticated: (obj) {
-              userGreeting = "Hey!";            // see above todo
-              signInOrChangeUser = "Anmelden";
-            }
-          );
-        return BackgroundImage(
-          imagePath: "assets/images/welcome_background_plain.jpg",
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: Center(
-                  child: Text(userGreeting, 
-                    style: TextStyle(
-                      fontSize: 60,
-                      color: Colors.white, 
-                    )
-                  )
-                  )
-                ),
-              Expanded(
-                flex: 5,
-                child: Center(
-                  child: Opacity(
-                    opacity: 0.9,
-                    child: Container(
-                      height: 100,
-                      width: 250,
-                      child: FlatButton(
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-                        color: Colors.green[500],
-                        onPressed: (){
-                          Router.navigator.pushReplacementNamed(Router.homePage);
-                        },
-                        child: Text("Los Gehts!",
-                          style: TextStyle(
-                            color: Colors.blueGrey[800],
-                            fontSize: 40,
-                          ),
-                        ),
-                      )
-                    )
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          if (state is Authenticated) {
+            var valueOrFailure = state.user.name.value.fold(
+              (f) => f,
+              (v) => v,
+            );
+            return BackgroundImage(
+              imagePath: "assets/images/welcome_background_plain.jpg",
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Expanded(
+                    flex: 2,
+                    child: userGreetingWidget("Hey, $valueOrFailure!"),
                   ),
+                  Expanded(
+                    flex: 5,
+                    child: Stack(alignment: Alignment.center, children: [
+                      goButton(),
+                      userProfileWidget("assets/images/user_profile.jpg"),
+                    ]),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: signInOrChangeUserWidget("Nutzer wechseln"),
                   )
-                ),
-              
-              Expanded(
-                flex: 1,
-                child: Center(
-                  child: FlatButton(
-                    color: Colors.blue[100],
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-                    child: Text(signInOrChangeUser, 
-                    style: TextStyle(
-                      color: Colors.blue[800],
-                      fontSize: 17,
-                    )
-                    )
-                    , onPressed: () {
-                      Router.navigator.pushReplacementNamed(Router.signInPage);
-                    },
-                    )
-                  )
-                ),
-            ],
+                ],
+              ),
+            );
+          }
+          if (state is Unauthenticated) {
+            return BackgroundImage(
+              imagePath: "assets/images/welcome_background_plain.jpg",
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Expanded(
+                    flex: 2,
+                    child: userGreetingWidget("Hey du!"),
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Stack(alignment: Alignment.center, children: [
+                      goButton(),
+                    ]),
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: signInOrChangeUserWidget("Nutzer wechseln")),
+                ],
+              ),
+            );
+          }
+          return Center(
+            child: Text("No state was called"),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget userGreetingWidget(String userGreeting) {
+    return Center(
+      child: Text(
+        userGreeting,
+        style: TextStyle(
+          fontSize: 60,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  Widget goButton() {
+    return Positioned(
+      child: Opacity(
+        opacity: 0.9,
+        child: Container(
+          height: 100,
+          width: 250,
+          child: FlatButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            color: Colors.green[500],
+            onPressed: () {
+              Router.navigator.pushReplacementNamed(Router.homePage);
+            },
+            child: Text(
+              "Los Gehts!",
+              style: TextStyle(
+                color: Colors.blueGrey[800],
+                fontSize: 40,
+              ),
+            ),
           ),
-        );
-        }
-      )
+        ),
+      ),
+    );
+  }
+
+  Widget userProfileWidget(userImagePath) {
+    return Positioned(
+      top: 5,
+      child: Container(
+        height: 200,
+        width: 200,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(userImagePath),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget signInOrChangeUserWidget(String message) {
+    return Center(
+      child: FlatButton(
+        color: Colors.blue[100],
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Text(
+          message,
+          style: TextStyle(
+            color: Colors.blue[800],
+            fontSize: 17,
+          ),
+        ),
+        onPressed: () {
+          Router.navigator.pushReplacementNamed(Router.signInPage);
+        },
+      ),
     );
   }
 }
