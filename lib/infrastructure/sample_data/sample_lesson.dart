05dffa3e1da602f33edfd6e028e95743df70a62e
Victor Reisenauer <victorreisenauer99@gmail.com>
2020-04-16 10:13:55 +0200

    feat: Adds sample data objects

---
+++ lib/infrastructure/sample_data/sample_lesson.dart
@@ -0,0 +1,24 @@
+import 'package:lrs_app_v3/domain/lesson/lesson.dart';
+import 'package:lrs_app_v3/domain/lesson/value_objects.dart';
+import 'package:lrs_app_v3/domain/core/value_objects.dart';
+import 'sample_exercise.dart';
+
+LessonList sampleLessonList = LessonList([sampleLesson1, sampleLesson2, sampleLesson3]);
+
+Lesson sampleLesson1 = Lesson(
+  exerciseList: ExerciseList(sampleExerciseList.value.fold(
+    (failure) => null,
+    (value) => value
+    )),
+  id: UniqueId(),
+  );
+
+Lesson sampleLesson2 = Lesson(
+  exerciseList: ExerciseList([sampleExercise2]),
+  id: UniqueId(),
+  );
+
+Lesson sampleLesson3 = Lesson(
+  exerciseList: ExerciseList([sampleExercise3, sampleExercise2, sampleExercise3, sampleExercise1]),
+  id: UniqueId(),
+  );
