5fba7d621e15f8706ea807a0c1bc04666d9f29b3
Jerry <jeremias1098@gmail.com>
2020-04-18 18:44:52 +0200

    feat: adds basic exercise_1/template_1 test

--- test/application/lesson/exercises/exercise_1_test.dart
+++ test/application/lesson/exercises/exercise_1_test.dart
@@ -1,1 +1,17 @@
-main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:lrs_app_v3/application/lesson/exerciseBlocs/exerciseBloc_1/exercise_1_bloc.dart';
+
+main() {
+  group("Exercise 1 Bloc => ", () {
+    test('emits initial state', () {
+      final bloc = Exercise_1Bloc();
+      expectLater(bloc, emits(Exercise_1State.initial()));
+      bloc.close();
+    });
+    test('emits showExercise state', () {
+      final bloc = Exercise_1Bloc();
+      bloc.add(Exercise_1Event.getExercise());
+      bloc.close();
+    });
+  });
+}
--- test/presentation/pages/exercise/templates/template_1_test.dart
+++ test/presentation/pages/exercise/templates/template_1_test.dart
@@ -1,1 +1,21 @@
-main() {}
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:lrs_app_v3/presenation/pages/exercise/templates/template_1/template_1.dart';
+
+main() {
+  group('Template1 testing => ', () {
+    test('Run app', () {
+      runApp(TestApp());
+    });
+  });
+}
+
+class TestApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Template1(),
+    );
+  }
+}
