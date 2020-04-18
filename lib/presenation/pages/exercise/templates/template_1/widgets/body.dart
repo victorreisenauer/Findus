8ac072ff15c9b1603ad82bc6e072359830de4360
Jerry <jeremias1098@gmail.com>
2020-04-18 19:04:00 +0200

    feat: adds template_1 basic widgets

---
+++ lib/presenation/pages/exercise/templates/template_1/widgets/body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Template_1Body extends StatefulWidget {
+  Template_1Body({Key key}) : super(key: key);
+
+  @override
+  _Template_1BodyState createState() => _Template_1BodyState();
+}
+
+class _Template_1BodyState extends State<Template_1Body> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Hello World'),
+    );
+  }
+}
