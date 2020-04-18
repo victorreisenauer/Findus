a3721fcdeabaca9469fcad6adecfbc4a5f574ff1
Jerry <jeremias1098@gmail.com>
2020-04-18 19:00:02 +0200

    fix: fixes mistake in exercise_1 test

--- lib/injection.iconfig.dart
+++ lib/injection.iconfig.dart
@@ -4,6 +4,7 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
+import 'package:lrs_app_v3/application/lesson/exerciseBlocs/exerciseBloc_1/exercise_1_bloc.dart';
 import 'package:lrs_app_v3/infrastructure/auth/test_auth_repository.dart';
 import 'package:lrs_app_v3/domain/auth/i_auth_facade.dart';
 import 'package:lrs_app_v3/infrastructure/auth/dev_auth_repository.dart';
@@ -12,7 +13,6 @@ import 'package:lrs_data_client/src/api.dart';
 import 'package:lrs_app_v3/infrastructure/core/api_injectable_module.dart';
 import 'package:lrs_app_v3/infrastructure/lesson/test_lesson_repository.dart';
 import 'package:lrs_app_v3/domain/lesson/i_lesson_facade.dart';
-import 'package:lrs_app_v3/application/lesson/exerciseBlocs/exerciseBloc_1/exercise_1_bloc.dart';
 import 'package:lrs_app_v3/application/auth/sign_in_form/sign_in_form_bloc.dart';
 import 'package:lrs_app_v3/application/auth/auth_bloc.dart';
 import 'package:lrs_app_v3/application/lesson/lesson_bloc.dart';
@@ -20,9 +20,9 @@ import 'package:get_it/get_it.dart';
 
 void $initGetIt(GetIt g, {String environment}) {
   final apiInjectableModule = _$ApiInjectableModule();
+  g.registerFactory<Exercise_1Bloc>(() => Exercise_1Bloc());
   g.registerLazySingleton<ApiUserMapper>(() => ApiUserMapper());
   g.registerLazySingleton<Api>(() => apiInjectableModule.api);
-  g.registerFactory<Exercise_1Bloc>(() => Exercise_1Bloc());
   g.registerFactory<SignInFormBloc>(() => SignInFormBloc(g<IAuthFacade>()));
   g.registerFactory<AuthBloc>(() => AuthBloc(g<IAuthFacade>()));
   g.registerFactory<LessonBloc>(() => LessonBloc(g<ILessonFacade>()));
--- test/presentation/pages/exercise/templates/template_1_test.dart
+++ test/presentation/pages/exercise/templates/template_1_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:lrs_app_v3/presenation/pages/exercise/templates/template_1/template_1.dart';
 
 main() {
   group('Template1 testing => ', () {
@@ -15,7 +14,7 @@ class TestApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Template1(),
+      body: Center(),
     );
   }
 }
