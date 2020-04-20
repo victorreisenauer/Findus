19b37cc32055e99e06398001bc000aecf1bc5cce
Jerry <jeremias1098@gmail.com>
2020-04-20 16:18:14 +0200

    test: adds cloud test

---
+++ test/presentation/pages/exercise/sharedWidgets/cloud.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:lrs_app_v3/presenation/pages/exercise/templates/sharedWidgets/cloud.dart';
+
+main() {
+  runApp(_CloudTestApp());
+}
+
+class _CloudTestApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            height: 250,
+            width: 350,
+            color: Colors.red,
+            child: Cloud(),
+          ),
+        ),
+      ),
+    );
+  }
+}
