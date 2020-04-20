3b9f514f4a3f171b9df2bc65ba06151397cd7664
Jerry <jeremias1098@gmail.com>
2020-04-20 16:16:33 +0200

    feat: adds basic functionality to Exercise_1Body

--- lib/presenation/pages/exercise/templates/template_1/widgets/body.dart
+++ lib/presenation/pages/exercise/templates/template_1/widgets/body.dart
@@ -11,9 +11,57 @@ class Template_1Body extends StatefulWidget {
 
 class _Template_1BodyState extends State<Template_1Body> {
   @override
+  void initState() {
+    BlocProvider.of<Exercise_1Bloc>(context).add(GetExercise());
+  }
+
+  @override
   Widget build(BuildContext context) {
     return BlocBuilder<Exercise_1Bloc, Exercise_1State>(
-      builder: (context, state) {},
+      builder: (context, state) {
+        if (state is ShowExercise) {
+          return Center(
+            child: Container(
+              height: 250,
+              width: 250,
+              child: Column(children: [
+                SingleChildScrollView(
+                  child: Column(children: [
+                    Wrap(
+                      children: _getWrapChildernFromText(state.text),
+                    ),
+                  ]),
+                ),
+                _getAnswers(state.answers),
+              ]),
+            ),
+          );
+        }
+        return CircularProgressIndicator();
+      },
     );
+  }
+
+  List<Widget> _getWrapChildernFromText(List<dynamic> text) {
+    List<Widget> widgets = List();
+    text.forEach((f) {
+      if (f[0] == 0) {
+        widgets.add(f[1]);
+      }
+      if (f[0] == 1) {
+        widgets.add(DragTarget(
+          builder: (context, list1, list2) {
+            return f[1];
+          },
+          onAccept: (data) => print(data),
+          onWillAccept: (data) => true,
+        ));
+      }
+    });
+    return widgets;
+  }
+
+  Widget _getAnswers(List<dynamic> answers) {
+    return Row();
   }
 }
