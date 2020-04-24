473389ed3ffe53ce7ce9ea526f8bebaaa0bdf7cd
Jerry <jeremias1098@gmail.com>
2020-04-24 20:05:48 +0200

    feat: adds clouds to overview page

--- lib/presenation/pages/overview/overview_page.dart
+++ lib/presenation/pages/overview/overview_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lrs_app_v3/domain/core/value_objects.dart';
 
 import 'package:lrs_app_v3/injection.dart';
 import '../../../application/lesson/lesson_bloc.dart';
@@ -11,11 +13,11 @@ class OverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocProvider(
-          create: (context) =>
-              getIt<LessonBloc>()..add(LessonEvent.fetchAllLessonIds()),
-          child: BackgroundImage(
-            imagePath: "assets/images/overview_background_plain.jpg",
+      body: Container(
+        color: Color.fromRGBO(166, 223, 249, 1.0),
+        child: BlocProvider(
+            create: (context) =>
+                getIt<LessonBloc>()..add(LessonEvent.fetchAllLessonIds()),
             child: BlocConsumer<LessonBloc, LessonState>(
               listener: (context, state) {
                 if (state is LessonStarted) {
@@ -34,7 +36,9 @@ class OverviewPage extends StatelessWidget {
                   );
                 }
                 if (state is AllLessonIdsLoaded) {
-                  return ListView.builder(
+                  return SizedBox.expand(
+                      child: _OverviewPageBody(ids: state.ids));
+                  /*ListView.builder(
                     padding: const EdgeInsets.all(8),
                     itemCount: state.ids.length,
                     itemBuilder: (BuildContext context, int index) {
@@ -53,13 +57,191 @@ class OverviewPage extends StatelessWidget {
                         },
                       );
                     },
-                  );
+                  );*/
                 }
                 return Center(child: Text("No state was called"));
               },
+            )),
+      ),
+    );
+  }
+}
+
+class _OverviewPageBody extends StatelessWidget {
+  List<UniqueId> ids;
+
+  _OverviewPageBody({this.ids});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constrains) {
+        return SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 125,
+                width: constrains.maxWidth,
+                child: CustomPaint(
+                  painter: _TopCloudPainter(),
+                ),
+              ),
+              _getCloudRowWithLessons(
+                  ids, BlocProvider.of<LessonBloc>(context)),
+              Container(
+                color: Colors.blue,
+                height: 400,
+                width: constrains.maxWidth,
+                child: CustomPaint(
+                  painter: _BottomPainter(),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+Widget _getCloudRowWithLessons(List<UniqueId> ids, LessonBloc bloc) {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: <Widget>[
+      Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: _getCloudColumnChildren(true, ids, bloc),
+      ),
+      Column(
+        children: _getCloudColumnChildren(false, ids, bloc),
+      ),
+    ],
+  );
+}
+
+List<Widget> _getCloudColumnChildren(
+    bool even, List<UniqueId> ids, LessonBloc bloc) {
+  List<Widget> widgets = List();
+  for (int i = even ? 0 : 1; i < ids.length; i += 2) {
+    widgets.add(_getCloudWithID(ids[i], i.toString(), bloc));
+  }
+  if (widgets.length == 0) widgets.add(Container());
+  return widgets;
+}
+
+Widget _getCloudWithID(UniqueId id, String name, LessonBloc bloc) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 30.0),
+    child: GestureDetector(
+        onTap: () {
+          bloc.add(StartLesson(id));
+        },
+        child: Container(
+          height: 100,
+          width: 175,
+          child: Cloud(
+              child: Center(
+            child: Text(
+              name,
+              style: GoogleFonts.reemKufi(
+                  fontSize: 20.0, fontWeight: FontWeight.bold),
             ),
           )),
-    );
+        )),
+  );
+}
+
+class _BottomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint green1 = Paint()
+      ..color = Color.fromRGBO(114, 198, 93, 1.0)
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 1.0;
+    Paint green2 = Paint()
+      ..color = Color.fromRGBO(108, 190, 88, 1.0)
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 1.0;
+    Paint green3 = Paint()
+      ..color = Color.fromRGBO(102, 178, 82, 1.0)
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 1.0;
+    Paint tree1 = Paint()
+      ..color = Color.fromRGBO(52, 159, 78, 1.0)
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 1.0;
+    Paint tree2 = Paint()
+      ..color = Color.fromRGBO(55, 168, 78, 1.0)
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 1.0;
+    Paint tribe = Paint()
+      ..color = Color.fromRGBO(159, 90, 60, 1.0)
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 1.0;
+
+    // TODO create bottom Painter
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class _TopCloudPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    print(size);
+    Paint cloudPaintDarkerSky = Paint()
+      ..color = Color.fromRGBO(213, 241, 254, 1)
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 1.0;
+
+    Paint cloudPaintLighterSky = Paint()
+      ..color = Color.fromRGBO(225, 245, 254, 1)
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 1.0;
+
+    Path darkerSky = Path();
+    darkerSky.moveTo(0.0, 0.0);
+    darkerSky.lineTo(size.width, 0.0);
+    darkerSky.lineTo(size.width, size.height * 0.6);
+
+    darkerSky.quadraticBezierTo(size.width * 0.85, size.height * 0.85,
+        size.width * 0.7, size.height * 0.6);
+    darkerSky.quadraticBezierTo(size.width * 0.655, size.height * 0.85,
+        size.width * 0.58, size.height * 0.65);
+    darkerSky.quadraticBezierTo(size.width * 0.42, size.height * 1.0,
+        size.width * 0.26, size.height * 0.65);
+    darkerSky.quadraticBezierTo(size.width * 0.19, size.height * 0.8,
+        size.width * 0.12, size.height * 0.6);
+    darkerSky.quadraticBezierTo(
+        size.width * 0.05, size.height * 0.75, 0.0, size.height * 0.6);
+
+    darkerSky.lineTo(0.0, 0.0);
+    canvas.drawPath(darkerSky, cloudPaintDarkerSky);
+
+    Path lighterSky = Path();
+    lighterSky.moveTo(0.0, 0.0);
+    lighterSky.lineTo(size.width, 0.0);
+    lighterSky.lineTo(size.width, size.height * 0.08);
+
+    lighterSky.quadraticBezierTo(size.width * 0.8, size.height * 0.8,
+        size.width * 0.6, size.height * 0.3);
+    lighterSky.quadraticBezierTo(size.width * 0.4, size.height * 0.8,
+        size.width * 0.2, size.height * 0.3);
+    lighterSky.quadraticBezierTo(
+        size.width * 0.1, size.height * 0.55, 0.0, size.height * 0.45);
+
+    lighterSky.lineTo(0.0, 0.0);
+
+    canvas.drawPath(lighterSky, cloudPaintLighterSky);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
 
