8c75cc8582d1c09c462b52273ae060480a3b8a7f
Jerry <jeremias1098@gmail.com>
2020-04-26 16:27:42 +0200

    feat: adds exercise_2

--- lib/presenation/pages/exercise/templates/template_2/exercise_2_temp.dart
+++ lib/presenation/pages/exercise/templates/template_2/exercise_2_temp.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lrs_app_v3/application/lesson/exercise/exercises/exerciseBloc_2/bloc/exercise_2_bloc.dart';
 
 import 'package:lrs_app_v3/domain/lesson/value_objects.dart';
 import 'package:lrs_app_v3/presenation/pages/exercise/templates/template.dart';
+import 'package:lrs_app_v3/presenation/pages/exercise/templates/template_2/widgets/body.dart';
 
 class Temp0002 extends Template {
   final ExerciseType type = ExerciseType('sampleType2');
@@ -11,6 +14,9 @@ class Temp0002 extends Template {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("This is Temp0002 consuming data $exerciseData"));
+    return BlocProvider(
+      create: (context) => Exercise_2Bloc(exerciseData: exerciseData),
+      child: Template_2Body(),
+    );
   }
 }
