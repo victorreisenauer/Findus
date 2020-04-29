4c15b4d69ce7feee6629ab12c469d8459f9a0fad
Victor Reisenauer <victorreisenauer99@gmail.com>
2020-04-29 20:32:35 +0200

    feat: Adds network info repo

--- lib/infrastructure/core/network_info.dart
+++ lib/infrastructure/core/network_info.dart
@@ -1,3 +1,14 @@
-abstract class NetworkInfo {
+import 'package:data_connection_checker/data_connection_checker.dart';
+
+abstract class NetworkInfoFacade {
   Future<bool> get isConnected;
+}
+
+class NetworkInfoRepository implements NetworkInfoFacade {
+  final DataConnectionChecker connectionChecker;
+
+  const NetworkInfoRepository(this.connectionChecker);
+
+  @override
+  Future<bool> get isConnected => connectionChecker.hasConnection;
 }
