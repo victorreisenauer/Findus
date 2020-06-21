39e38e912089deb83d0c3b4ed54c77aa0cecb565
Jerry <jeremias1098@gmail.com>
2020-06-21 15:39:21 +0200

    feat: changes remote_datasource to use new lesson_model

--- lib/infrastructure/lesson/data_sources/firebase/remote_firebase_lesson_data_source.dart
+++ lib/infrastructure/lesson/data_sources/firebase/remote_firebase_lesson_data_source.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:injectable/injectable.dart';
+import 'package:lrs_app_v3/domain/core/value_objects_barrel.dart';
 import 'package:lrs_app_v3/infrastructure/lesson/data_sources/remote_lesson_data_source_facade.dart';
 import 'package:lrs_app_v3/infrastructure/lesson/lesson_barrel.dart';
 
@@ -19,11 +20,17 @@ class RemoteFirebaseLessonDataSource implements RemoteLessonDataSourceFacade {
   ///
   /// Missing implementation!
   Stream<LessonModel> getAvailableLessonData() async* {
-    String response = await _cloudFunctions
-        .getHttpsCallable(functionName: "getLessons")
-        .call()
-        .then((response) => response.data);
-    yield LessonModel.fromJson(jsonDecode(response));
+    Map response =
+        await _cloudFunctions.getHttpsCallable(functionName: "getLessons").call().then((response) => response.data);
+
+    // converts Map<dynamic, dynamic> to Map<String, dynamic>
+    // fixes some types in Json
+    response = jsonDecode(jsonEncode(response));
+    LessonModels models = LessonModels.fromJson(response);
+
+    for (LessonModel model in models.exercises) {
+      yield model;
+    }
   }
 
   /// Pushes a [resultModel] to server.
